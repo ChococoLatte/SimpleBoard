45cd6d0d3d276d98e0224c9fe7bd34bf9da55549
youngju kim <eorhrtnv@naver.com>
2025-02-04 10:32:56 +0900

    fix: 타임화면과의 연동 수정

--- src/main/java/com/example/simple_board/controller/BoardController.java
+++ src/main/java/com/example/simple_board/controller/BoardController.java
@@ -4,6 +4,8 @@ import com.example.simple_board.dto.BoardDTO;
 import com.example.simple_board.service.BoardServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,7 +15,8 @@ public class BoardController {
     private final BoardServiceImpl boardService;
 
     @PostMapping("/save")
-    public void save(@RequestBody BoardDTO boardDTO) {
+    public String save(@ModelAttribute BoardDTO boardDTO) {
         boardService.save(boardDTO);
+        return "save";
     }
 }
